3349fc7eccd0f5eb13159e7d88a0a4b357be31c7
xeemv <xeeyang08@gmail.com>
2022-02-25 16:02:21 -0600

    made some changes

--- js/script.js
+++ js/script.js
@@ -11,10 +11,12 @@
     - a Bootstrap alert or similar Bootstrap 
     - component should appear across the screen announcing the winner.
 */
+
+
 var turn = document.getElementById("turn"),
-boxes = document.querySelectorAll("#main div");
-let X_or_O = 0;
+boxes = document.querySelectorAll("#main .box");
 // use query selector to select all of my classes in from main div
+let X_or_O = 0;
 var b0 = document.getElementById("b0"),
     b1 = document.getElementById("b1"),
     b2 = document.getElementById("b2"),
@@ -27,6 +29,14 @@ var b0 = document.getElementById("b0"),
 // using dom getElementById to find all my boxes
 
 
+newGame();
+
+function newGame(){
+    startGame();
+}
+
+
+// check the winning combo to declare a winner
 function winningBoxes(b0, b1, b2) {
     b0.classList.add("win");
     // using classlist property to check if the winning box is box0 from the top row
@@ -44,13 +54,17 @@ function winningBoxes(b0, b1, b2) {
     // the add method to pull the css "win" style and add it to the class list for the box0
      // will highlight the boxes that are a part of the winning connections
     turn.innerHTML = b0.innerHTML + " is the winner!!! Woohoo, time to celebrate. Play again!!";
+    console.log(b0.innerHTML + " is the winner!!! Woohoo, time to celebrate. Play again!!");
     // this will display who's won the game
     turn.style.fontSize = "30px";
     // turn.innerHTML = 'Play again';
   }
-  console.log(b1.classList);
+  console.log(b0.classList);
 
-  function checkWinner() {
+
+
+// check for the winning combos
+function checkWinner() {
     // ways for how a game could be won
     if (b0.innerHTML !== "" && b0.innerHTML === b1.innerHTML && b0.innerHTML === b2.innerHTML)
       winningBoxes(b0, b1, b2);
@@ -75,7 +89,7 @@ function winningBoxes(b0, b1, b2) {
     /*================*/
     if (b6.innerHTML !== "" && b6.innerHTML === b7.innerHTML && b6.innerHTML === b8.innerHTML)
         winningBoxes(b6, b7, b8);
-} 
+}
 //   console.log(X_or_O + "won this row");
 
 
@@ -85,33 +99,33 @@ function startGame(){
             for (var i = 0; i < boxes.length; i++) {
                 boxes[i].onclick = function () {
                 if (this.innerHTML !== "X" && this.innerHTML !== "O") {
-                    // clean board w/ no X or O
+                    // clean board w/ no X or O starting
                     if (X_or_O % 2 === 0) {
                     console.log(X_or_O);
                     this.innerHTML = "X";
                     // if turn started w/ X
                     // players are defined by this line
-                    turn.innerHTML = "O Turn Now";
+                    turn.innerHTML = "Player 2 it is your turn now";
                     //  will pop up to tell "O" to go next
                     checkWinner();
                     // check to see if this select caused a win
                     X_or_O += 1;
-                    console.log("X, you got this.");
+                    // console.log("X, you got this.");
                     //  print encouraging message
                 } else {
                     console.log(X_or_O);
                     this.innerHTML = "O";
-                    turn.innerHTML = "X Turn Now";
+                    turn.innerHTML = "Player 1 it is your turn now";
                     checkWinner();
                     // check to see if this select caused a win
                     X_or_O += 1;
-                    console.log("I hope you win O!!");
+                    // console.log("I hope you win O!!");
                     //  print encouraging message
                     }
                 } 
-            }  
+            } 
         };
-    }
+}
 
 
 document.getElementById('replay').addEventListener('click', replay);
@@ -126,23 +140,26 @@ function replay() {
       boxes[i].classList.remove("win");
     // remove the winning css style
       boxes[i].innerHTML = "";
-    // the box will clear and no x/o will be displayed
+    // all the boxes will clear and no x/o will be displayed
       turn.innerHTML = "Opposite player start";
+    // this will prompt the opposite player to start the next round
       turn.style.fontSize = "25px";
+      console.log(X_or_O);
+    // will display how many clicks
     }
 }
 
-// function isDraw() {
-//     if (boxes.length )
-//     });
-// }
 
-
-
-
-
-function newGame(){
-    startGame();
-}
-
-newGame();
+function endGame(draw) {
+    if (draw) {
+      turn.innerText = 'Draw!'
+    } else {
+      turn.innerText = b0.innerHTML + " is the winner!!! Woohoo, time to celebrate. Play again!!";
+    }
+  }
+  
+  function isDraw() {
+    return [...boxes].every(box => {
+      return box.classList.contains("O") || box.classList.contains("X")
+    });
+  }
